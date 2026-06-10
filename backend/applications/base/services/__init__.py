8a36f15f26b8226694c65df0a0a179d66e6ff5d9
KKNOBUG <807440781@qq.com>
2026-06-10 17:04:42 +0800

    ...

--- backend/applications/base/services/__init__.py
+++ backend/applications/base/services/__init__.py
@@ -6,29 +6,11 @@
 @Module  : __init__.py
 @DateTime: 2025/4/28 18:07
 """
-# from backend.applications.base.services.audit_crud import AuditCrud
-# from backend.applications.base.services.scaffold import (
-#     unique_identify,
-#     ScaffoldModel,
-#     UUIDModel,
-#     PacketModel,
-#     StateModel,
-#     ClassModel,
-#     TimestampMixin,
-#     MaintainMixin,
-#     ReserveFields,
-#     ScaffoldCrud,
-#     QueryBuilder,
-#     UpperStr,
-#     LowerStr,
-#
-# )
-#
-# __all__ = [
-#     "AuditCrud",
-#     "unique_identify", "ScaffoldModel", "UUIDModel",
-#     "PacketModel", "StateModel", "ClassModel",
-#     "TimestampMixin", "MaintainMixin", "ReserveFields",
-#     "ScaffoldCrud", "QueryBuilder", "UpperStr",
-#     "LowerStr",
-# ]
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Python')
